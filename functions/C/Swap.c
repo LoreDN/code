7aaea1c8c4d29d07a19b68daf8589f2f67ed5409
Lorenzo Di Napoli <loridinapoli@gmail.com>
2025-02-28 12:41:51 +0100

    Add Swap Function

---
+++ functions/C/Swap.c
@@ -0,0 +1,14 @@
+/* swap */
+void Swap(int *a, int *b)
+{
+    /* variable initialization */
+    int temp;
+
+    /* swap */
+    temp = *a;
+    *a = *b;
+    *b = temp;
+
+    /* exit */
+    return;
+}
